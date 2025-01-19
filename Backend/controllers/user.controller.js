2d40d11450b62f38a36714edd53ca32eb882bf40
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-01-19 10:30:41 +0530

    add login route

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -8,14 +8,14 @@ module.exports.registerUser = async( req, res, next)=>{
     if(!errors.isEmpty()){
         return res.status(400).json({errors: errors.array() });
     }
-
-    const {firstname, lastname, email, password} = req.body;
+    console.log(req.body);
+    const {fullname, email, password} = req.body;
 
     const hashedPassword= await userModel.hashPassword(password);
 
     const user= await userService.createUser({
-        firstname,
-        lastname,
+        firstname: fullname.firstname,
+        lastname: fullname.lastname,
         email,
         password: hashedPassword
     });
@@ -23,5 +23,28 @@ module.exports.registerUser = async( req, res, next)=>{
     const token = user.generateAuthToken();
 
     res.status(201).json({ token, user});
-    
+
+}
+
+module.exports.loginUser = async(req, res, next)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors:errors.array() });
+    }
+
+    const {email, password} = req.body;
+    const user = await userModel.findOne({ email}).select('+password');
+
+    if(!user){
+        return res.status(401).json({message: 'Invalid email or phone'});
+    }
+
+    const isMatch = await user.comparePassword(password);
+
+    if(!isMatch){
+        return res.status(401).json({message: 'Invalid email or password'});
+
+    }
+    const token = user.generateAuthToken();
+    res.status(200).json({token,user});
 }
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -3,11 +3,16 @@ const router = express.Router();
 const { body } = require("express-validator");
 const userController= require('../controllers/user.controller');
 
-
+//registration route
 router.post('/register', [
     body('email').isEmail().withMessage('Invalid Email'),
     body('fullname.firstname').isLength({ min:3}).withMessage('First name must be atleast 3 characters long'),
-    body('password').isLength({min: 6}),withMessage('Password must be atleast 6 characters long')
+    body('password').isLength({min: 6}).withMessage('Password must be atleast 6 characters long')
 ], userController.registerUser)
 
+//login route
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').notEmpty().withMessage('Password is required')
+], userController.loginUser );
 module.exports = router;

7caad16af45dafc6865e374a7edb9b0d9b15b3b8
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-02-02 10:57:50 +0530

    captain logout and profile route

--- Backend/controllers/captain.controller.js
+++ Backend/controllers/captain.controller.js
@@ -1,3 +1,4 @@
+const blacklistTokenModel = require('../models/blacklistToken.model');
 const captainModel= require('../models/captain.model');
 const captainService = require('../services/captain.service');
 const {validationResult} = require('express-validator');
@@ -14,7 +15,7 @@ module.exports.registerCaptain = async (req, res, next)=> {
     const isCaptainAlreadyExist = await captainModel.findOne({ email });
 
     if (isCaptainAlreadyExist) {
-        return res.status(400).json({ message: 'Captain already exist' });
+        return res.status(400).json({ message: 'Captain already exists' });
     }
 
     const hashedPassword = await captainModel.hashedPassword(password);
@@ -35,3 +36,43 @@ module.exports.registerCaptain = async (req, res, next)=> {
     res.status(201).json({ token, captain });
 
 }
+
+module.exports.loginCaptain = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    const captain = await captainModel.findOne({ email }).select('+password');
+
+    if (!captain) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const isMatch = await captain.comparePassword(password);
+
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    const token = captain.generateAuthToken();
+
+    res.cookie('token', token);
+
+    res.status(200).json({ token, captain });
+}
+
+module.exports.getCaptainProfile = async(req, res, next)=>{
+    res.status(200).json({ captain: req.captain});
+}
+
+module.exports.logoutCaptain = async(req,res,next)=>{
+    const token= req.cookies.token;
+    await blacklistTokenModel.create({token});
+
+    res.clearCookie('token');
+    res.status(200).json({message: 'Logout successful'});
+}
+

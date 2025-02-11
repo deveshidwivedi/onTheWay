3a1cdc4f25352c1be5d0f3ac2f7ad82aaeadd8e9
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-02-11 13:29:29 +0530

    user profile fetch

--- Frontend/src/pages/UserProtectedWrapper.jsx
+++ Frontend/src/pages/UserProtectedWrapper.jsx
@@ -9,10 +9,36 @@ const UserProtectWrapper = ({
     const token = localStorage.getItem('token')
     const navigate = useNavigate()
 
-    if (!token) {
-        navigate('/login')
-    }
+    const { user, setUser } = useContext(UserDataContext)
+    const [isLoading, setIsLoading] = useState(true)
 
+    useEffect(() => {
+        if (!token) {
+            navigate('/login')
+        }
+
+        axios.get(`${import.meta.env.VITE_BASE_URL}/users/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            if (response.status === 200) {
+                setUser(response.data)
+                setIsLoading(false)
+            }
+        })
+            .catch(err => {
+                console.log(err)
+                localStorage.removeItem('token')
+                navigate('/login')
+            })
+    }, [token])
+
+    if (isLoading) {
+        return (
+            <div>Loading...</div>
+        )
+    }
 
     return (
         <>

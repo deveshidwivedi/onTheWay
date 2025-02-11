0e9cff9e0d82567c0c41d4cc7e5cd9dfcc664a0a
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-02-11 09:10:43 +0530

    add start

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import './index.css';
-import {Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Start from './pages/Start';
 import UserLogin from './pages/UserLogin';
 import UserSignup from './pages/UserSignup';
 import CaptainLogin from './pages/Captainlogin';
@@ -9,13 +10,14 @@ import CaptainSignup from './pages/Captainsignup';
 
 const App = () => {
   return (
-    <div> 
+    <div>
       <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<UserLogin/>} />
-        <Route path="/signup" element={<UserSignup/>} />
-        <Route path="/captain-login" element={<CaptainLogin/>} />
-        <Route path="/captain-signup" element={<CaptainSignup/>} />
+        <Route path="/" element={<Start />} />
+        <Route path="/login" element={<UserLogin />} />
+        <Route path="/signup" element={<UserSignup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+        <Route path='/home' element={<Home />} />
       </Routes>
     </div>
   )
--- Frontend/src/pages/UserSignup.jsx
+++ Frontend/src/pages/UserSignup.jsx
@@ -1,94 +1,94 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 
 const UserSignup = () => {
-        const [email, setEmail] = useState('');
-        const [password, setPassword] = useState('');
-        const [firstName, setFirstName] = useState('');
-        const [lastName, setLastName] = useState('');
-        const [userData, setUserData] = useState({});
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [userData, setUserData] = useState({});
 
-        const submitHandler = (e) => {
-            e.preventDefault();
-            setUserData({
-                fullName:{
-                    firstName: firstName,
-                    lastName: lastName
-                },
-                email: email,
-                password: password
-            })
-            //reset 
-            setEmail('');
-            setPassword('');
-            setFirstName('');
-            setLastName('');
-           
-        }
-    
-    
+    const submitHandler = (e) => {
+        e.preventDefault();
+        setUserData({
+            fullName: {
+                firstName: firstName,
+                lastName: lastName
+            },
+            email: email,
+            password: password
+        })
+        //reset 
+        setEmail('');
+        setPassword('');
+        setFirstName('');
+        setLastName('');
+
+    }
+
+
     return (
         <div className="p-7 flex flex-col justify-between h-screen">
-        <div>
-        <img className="w-25 -ml-2 -mt-8 -mb-4" src={logo} alt="Logo"  />
-         
-          <form onSubmit={(e)=> {
-           submitHandler(e);
-          }}>
+            <div>
+                <img className="w-25 -ml-2 -mt-8 -mb-4" src={logo} alt="Logo" />
 
-        <h3 className="text-lg font-medium mb-2"> Your good name?</h3>
-           <div className="flex gap-2">
-           <input required
-           className="bg-[#eeeeee] w-1/2 mb-2 rounded px-4 py-2 border  text-lg placeholder:text-base"
-           type="text" placeholder="First name" 
-           value={firstName}
-           onChange={(e)=>{
-            setFirstName(e.target.value);
-           }}
-           />
-           <input required
-           className="bg-[#eeeeee] w-1/2 mb-2 rounded px-4 py-2 border   text-lg placeholder:text-base"
-           type="text" placeholder="Last name"
-           value={lastName}
-           onChange={(e)=>{
-            setLastName(e.target.value);
-           }}
-           />
-           </div>
+                <form onSubmit={(e) => {
+                    submitHandler(e);
+                }}>
 
-           <h3 className="text-lg font-medium mb-2"> Your email address, please.</h3>
-           <input required
-           className="bg-[#eeeeee] mb-5 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
-           type="email" placeholder="email@example.com" 
-           value={email}
-           onChange={(e)=>{
-            setEmail(e.target.value);
-           }}
-           />
+                    <h3 className="text-lg font-medium mb-2"> Your good name?</h3>
+                    <div className="flex gap-2">
+                        <input required
+                            className="bg-[#eeeeee] w-1/2 mb-2 rounded px-4 py-2 border  text-lg placeholder:text-base"
+                            type="text" placeholder="First name"
+                            value={firstName}
+                            onChange={(e) => {
+                                setFirstName(e.target.value);
+                            }}
+                        />
+                        <input required
+                            className="bg-[#eeeeee] w-1/2 mb-2 rounded px-4 py-2 border   text-lg placeholder:text-base"
+                            type="text" placeholder="Last name"
+                            value={lastName}
+                            onChange={(e) => {
+                                setLastName(e.target.value);
+                            }}
+                        />
+                    </div>
 
-           <h3 className="text-lg font-medium mb-2">Enter a secure password.</h3>
-           <input required
-           className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
-           type="password" placeholder="password"
-           value={password}
-           onChange={(e)=>{
-            setPassword(e.target.value);
-           }}
-           />
+                    <h3 className="text-lg font-medium mb-2"> Your email address, please.</h3>
+                    <input required
+                        className="bg-[#eeeeee] mb-5 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+                        type="email" placeholder="email@example.com"
+                        value={email}
+                        onChange={(e) => {
+                            setEmail(e.target.value);
+                        }}
+                    />
 
-          <button  className="bg-[#111] text-white font-semibold mb-2 rounded px-4 py-2 w-full text-lg">Login</button>
+                    <h3 className="text-lg font-medium mb-2">Enter a secure password.</h3>
+                    <input required
+                        className="bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placeholder:text-base"
+                        type="password" placeholder="password"
+                        value={password}
+                        onChange={(e) => {
+                            setPassword(e.target.value);
+                        }}
+                    />
 
-          <p className="text-center">
-           Already have an account? <Link to="/login" className="text-blue-600">Login here</Link>
-           </p>
+                    <button className="bg-[#111] text-white font-semibold mb-2 rounded px-4 py-2 w-full text-lg">Signup</button>
 
-           </form>
+                    <p className="text-center">
+                        Already have an account? <Link to="/login" className="text-blue-600">Login here</Link>
+                    </p>
+
+                </form>
+            </div>
+            <div>
+                <p className="text-[10px] leading-tight">By proceeding, you consent to get calls, WhatsApp or SMS messages, including by automated means, from onTheWay to the number you provided.</p>
+            </div>
         </div>
-        <div>
-           <p className="text-[10px] leading-tight">By proceeding, you consent to get calls, WhatsApp or SMS messages, including by automated means, from onTheWay to the number you provided.</p>
-        </div>
-       </div>
     )
 }
 

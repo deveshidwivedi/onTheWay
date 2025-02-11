a0e277e18f21145988c2d781cc24fbe9dc7cbc5b
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-02-11 13:27:31 +0530

    captain profile fetch

--- Frontend/src/pages/CaptainProtectWrapper.jsx
+++ Frontend/src/pages/CaptainProtectWrapper.jsx
@@ -8,10 +8,39 @@ const CaptainProtectWrapper = ({
 }) => {
     const token = localStorage.getItem('token')
     const navigate = useNavigate()
+    const { captain, setCaptain } = useContext(CaptainDataContext)
+    const [isLoading, setIsLoading] = useState(true)
 
-    if (!token) {
-        navigate('/captain-login')
+    useEffect(() => {
+        if (!token) {
+            navigate('/captain-login')
+        }
+
+        axios.get(`${import.meta.env.VITE_BASE_URL}/captains/profile`, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then(response => {
+            if (response.status === 200) {
+                setCaptain(response.data.captain)
+                setIsLoading(false)
+            }
+        })
+            .catch(err => {
+
+                localStorage.removeItem('token')
+                navigate('/captain-login')
+            })
+    }, [token])
+
+
+
+    if (isLoading) {
+        return (
+            <div>Loading...</div>
+        )
     }
+
 
 
     return (

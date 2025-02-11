14de8ea96c58f1e5ed88689de7daeb62fb495c0d
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-02-11 09:52:23 +0530

    user sign up form submission with api integ

--- Frontend/src/pages/UserSignup.jsx
+++ Frontend/src/pages/UserSignup.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../assets/logo.png";
+import axios from 'axios';
+import { UserDataContext } from '../context/UserContext';
+
 
 const UserSignup = () => {
     const [email, setEmail] = useState('');
@@ -9,16 +12,35 @@ const UserSignup = () => {
     const [lastName, setLastName] = useState('');
     const [userData, setUserData] = useState({});
 
-    const submitHandler = (e) => {
+    const navigate = useNavigate();
+    const { user, setUser } = React.useContext(UserDataContext);
+
+
+    const submitHandler = async (e) => {
         e.preventDefault();
-        setUserData({
-            fullName: {
-                firstName: firstName,
-                lastName: lastName
+        const newUser = {
+            fullname: {
+                firstname: firstName,
+                lastname: lastName
             },
             email: email,
             password: password
-        })
+        }
+
+        const response = await fetch(`${import.meta.env.VITE_BASE_URL}/users/register`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newUser)
+        });
+
+        if (response.status === 201) {
+            const data = await response.json();
+            setUser(data);
+            navigate('/home');
+        }
+
         //reset 
         setEmail('');
         setPassword('');
@@ -77,7 +99,7 @@ const UserSignup = () => {
                         }}
                     />
 
-                    <button className="bg-[#111] text-white font-semibold mb-2 rounded px-4 py-2 w-full text-lg">Signup</button>
+                    <button className="bg-[#111] text-white font-semibold mb-2 rounded px-4 py-2 w-full text-lg">Create Account</button>
 
                     <p className="text-center">
                         Already have an account? <Link to="/login" className="text-blue-600">Login here</Link>

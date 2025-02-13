1b040a9396616a7a462e05959fde6030fd6a676b
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-02-13 16:41:17 +0530

    new component for vehicle panel

---
+++ Frontend/src/components/VehiclePanel.jsx
@@ -0,0 +1,52 @@
+import React, { useState, useRef } from 'react';
+import logo from "../assets/logo.png";
+import bg22 from "../assets/bg22.jpg";
+import { useGSAP } from "@gsap/react";
+import 'remixicon/fonts/remixicon.css'
+import gsap from "gsap";
+import LocationSearchPanel from '../components/LocationSearchPanel';
+import car from '../assets/car.png';
+import bike from '../assets/bike.png';
+import auto from '../assets/auto.png';
+const VehiclePanel = (props) => {
+    return (
+        <div>
+            <h5 className='p-1 text-center w-[93%] absolute top-0  '
+                onClick={() => {
+                    props.setVehiclePanelOpen(false);
+                }}
+            ><i className='text-3xl text-gray-200 ri-arrow-down-wide-line'></i></h5>
+            <h3 className='text-xl font-semibold mb-5'>Choose your ride</h3>
+            <div className="flex bg-gray-100 rounded-xl w-full p-3 items-center justify-between mb-2 active:border-2 active:border-black">
+                <img className='h-20' src={car} alt='car' />
+                <div className='w-1/2'>
+                    <h4 className='font-medium text-base'>OTWgo <span><i className="ri-user-3-fill"></i>4</span></h4>
+                    <h5 className='font-medium text-sm'>2 mins away</h5>
+                    <p className='font-medium text-xs text-gray-600'>Pocket-friendly rides</p>
+                </div>
+                <h2 className=' text-xl font-semibold'> ₹208.6</h2>
+            </div>
+            <div className="flex bg-gray-100 rounded-xl w-full p-3 items-center justify-between mb-2 active:border-2 active:border-black">
+                <img className='h-20' src={bike} alt='car' />
+                <div className='w-1/2'>
+                    <h4 className='font-medium text-base'>Moto <span><i className="ri-user-3-fill"></i>1</span></h4>
+                    <h5 className='font-medium text-sm'>10 mins away</h5>
+                    <p className='font-medium text-xs text-gray-600'>Zip through traffic</p>
+                </div>
+                <h2 className=' text-xl font-semibold'> ₹110.5</h2>
+            </div>
+            <div className="flex bg-gray-100 rounded-xl w-full p-3 items-center justify-between mb-2 active:border-2 active:border-black">
+                <img className='h-17' src={auto} alt='car' />
+                <div className='w-1/2'>
+                    <h4 className='font-medium text-base'>OTWauto<span><i className="ri-user-3-fill"></i>3</span></h4>
+                    <h5 className='font-medium text-sm'>5 mins away</h5>
+                    <p className='font-medium text-xs text-gray-600'>Hop in, ride cheap</p>
+                </div>
+                <h2 className=' text-xl font-semibold'> ₹156.2</h2>
+            </div>
+
+        </div>
+    )
+}
+
+export default VehiclePanel;

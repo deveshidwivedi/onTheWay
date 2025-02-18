7affdde3da845cc8554c074acf29fd8f423a25da
Deveshi Dwivedi <deveshigurgaon@gmail.com>
2025-02-18 20:54:18 +0530

    ride route, service and controller

---
+++ Backend/routes/ride.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const {body} = require('express-validator');
+const rideController = require('../controllers/ride.controller');
+
+router.post('/create',
+    body('userId').isString().isLength({min: 24, max: 24}).withMessage('Invalid user id'),
+    body('pickup').isString().isLength({min: 3}).withMessage('Invalid pickup location'),
+    body('destination').isString().isLength({min: 3}).withMessage('Invalid drop location'),
+    body('vehicleType').isString().isIn(['auto', 'car', 'motorcycle']).withMessage('Invalid vehicle type'),
+    rideController.createRide
+)
